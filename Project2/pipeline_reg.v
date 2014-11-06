be11ce340db4e6c0416b1719ba3771ff2c9b582e
chrisenuf <chrisenuf@gmail.com>
2014-11-06 18:55:44 -0500

    compiles, can't say if it works or not, lots of random quartus files being deleted and added

--- Project2/pipeline_reg.v
+++ Project2/pipeline_reg.v
@@ -1,25 +1,27 @@
-module pipeline_reg(clk, reset, buffWrEn, regWrEn, memWren, mulSel, aluOut, PC, instrType, branchEn, memWrenOut, mulSelOut, aluOutOut, PCOut, instrTypeOut, branchEnOut);
+module pipeline_reg(clk, reset, buffWrEn, regWrEn, regWrAddr, memWrEn, mulSel, aluOut, PC, isLoad, isStore, regWrEnOut, regWrAddrOut, memWrEnOut, mulSelOut, aluOutOut, PCOut, isLoadOut, isStoreOut);
 	parameter BIT_WIDTH = 32;
 
 	input clk, reset;
-	input regWrEn, memWren, branchEn;
+	input buffWrEn;
+	input regWrEn, memWrEn, isLoad, isStore;
 	input [1:0] mulSel;
-	input [BIT_WIDTH - 1] aluOut, PC;
-	input [3:0] instrType; //not sure about this
+	input [3:0] regWrAddr;
+	input [BIT_WIDTH - 1:0] aluOut, PC;
 	
-	output reg regWrEnOut, memWrenOut, branchEnOut;
-	output reg [1:0] mulSelOut;
-	output reg [BIT_WIDTH - 1] aluOutOut, PCOut;
-	output reg [3:0] instrTypeOut; //not sure about this
+	output regWrEnOut, memWrEnOut, isLoadOut, isStoreOut;
+	output [1:0] mulSelOut;
+	output [3:0] regWrAddrOut;
+	output [BIT_WIDTH - 1:0] aluOutOut, PCOut;
 	//I think we also need something for the register file write addr
 	
 	bufRegister #(.BIT_WIDTH(1)) regWrEnBuff (.clk(clk), .reset(reset), .wrtEn(buffWrEn), .dataIn(regWrEn), .dataOut(regWrEnOut));
 	bufRegister #(.BIT_WIDTH(1)) memWrEnBuff (.clk(clk), .reset(reset), .wrtEn(buffWrEn), .dataIn(memWrEn), .dataOut(memWrEnOut));
-	bufRegister #(.BIT_WIDTH(1)) branchEnBuff (.clk(clk), .reset(reset), .wrtEn(buffWrEn), .dataIn(branchEn), .dataOut(branchEnOut));
+	bufRegister #(.BIT_WIDTH(1)) isStoreBuff (.clk(clk), .reset(reset), .wrtEn(buffWrEn), .dataIn(isStore), .dataOut(isStoreOut));
+	bufRegister #(.BIT_WIDTH(1)) isLoadBuff (.clk(clk), .reset(reset), .wrtEn(buffWrEn), .dataIn(isLoad), .dataOut(isLoadOut));
 	
 	bufRegister #(.BIT_WIDTH(2)) mulSelBuff (.clk(clk), .reset(reset), .wrtEn(buffWrEn), .dataIn(mulSel), .dataOut(mulSelOut));
 	
-	bufRegister #(.BIT_WIDTH(4)) instrTypeBuff (.clk(clk), .reset(reset), .wrtEn(buffWrEn), .dataIn(instrType), .dataOut(instrTypeOut));
+	bufRegister #(.BIT_WIDTH(4)) regWrAddrBuff (.clk(clk), .reset(reset), .wrtEn(buffWrEn), .dataIn(regWrAddr), .dataOut(regWrAddrOut));
 	
 	bufRegister #(.BIT_WIDTH(BIT_WIDTH)) aluOutBuff (.clk(clk), .reset(reset), .wrtEn(buffWrEn), .dataIn(aluOut), .dataOut(aluOutOut));
 	bufRegister #(.BIT_WIDTH(BIT_WIDTH)) PCBuff (.clk(clk), .reset(reset), .wrtEn(buffWrEn), .dataIn(PC), .dataOut(PCOut));
